53e746b9ec9f48c43a5e37006a540c3096aa93f5
Matt Raines <mraines4@gmail.com>
2019-02-20 22:26:09 -0500

    created Pipes class (issues with adding different pipes from list)

--- app.py
+++ app.py
@@ -17,17 +17,25 @@ class DC(object):
         self.y += self.dir_y
 
 # creates Pipe class
-# class Pipes(object):
-#     def __init__(self):
-#         self.image = pygame.image.load('images/DC-logo.png').convert_alpha()
-#         self.x = 60
-#         self.y = 400
-#         self.dir_y = 20
+class Pipes(object):
+    def __init__(self):
+        self.image = pygame.image.load('images/pipes.png').convert_alpha()
+        self.x = 1000
+        self.y = 0
+        self.dir_x = -20
 
-#     def draw(self, surface):
-#         surface.blit(self.image, (self.x, self.y))
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
 
-# pipe_list = []
+    def update(self):
+        self.x += self.dir_x
+
+pipe_list = [
+    # Pipes(-400),
+    # Pipes(100),
+    # Pipes(-200),
+    # Pipes(-300)
+]
 
 
 def main():
@@ -41,9 +49,11 @@ def main():
 
     # Game initialization (prints background image/pipes/and DC)
     background_image = pygame.image.load('images/background.png').convert_alpha()
-    pipes = pygame.image.load('images/pipes.png').convert_alpha()
+    # pipes = pygame.image.load('images/pipes.png').convert_alpha()
     dc_logo = DC()
-    # pipe = Pipes(pipe_list)
+    # for pipe in pipe_list:
+    #     moving_pipe = Pipes()
+    moving_pipe = Pipes()
 
     stop_game = False
     while not stop_game:
@@ -78,12 +88,14 @@ def main():
         # Game display
         dc_logo.draw(screen)
         dc_logo.update()
+        moving_pipe.draw(screen)
+        moving_pipe.update()
 
         # dc_logo.handle_keys()
-        screen.blit(pipes, (0,-400)),
-        screen.blit(pipes, (100,-100)),
-        screen.blit(pipes, (200,-200)),
-        screen.blit(pipes, (300,-300))
+        # screen.blit(pipes, (0,-400)),
+        # screen.blit(pipes, (100,-100)),
+        # screen.blit(pipes, (200,-200)),
+        # screen.blit(pipes, (300,-300))
         pygame.display.update()
         clock.tick(60)
 
