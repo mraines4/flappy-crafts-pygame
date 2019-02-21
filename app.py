369e3fe720acc31c65ece6509ee4f18aeac166ec
Matt Raines <mraines4@gmail.com>
2019-02-20 21:51:58 -0500

    fixes keystroke issue and adds comments

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 import pygame
 
+KEY_UP = 273
+
+# create DC class
 class DC(object):
     def __init__(self):
         self.image = pygame.image.load('images/DC-logo.png').convert_alpha()
         self.x = 60
         self.y = 400
-        self.dir_y = 10
-
-    def handle_keys(self):
-        key = pygame.key.get_pressed()
-        dist = 40
-        if key[pygame.K_UP]:
-            self.y -= dist
-            self.image = pygame.image.load('images/DC-logo-tilt.png').convert_alpha()
+        self.dir_y = 20
 
     def draw(self, surface):
         surface.blit(self.image, (self.x, self.y))
@@ -26,7 +22,7 @@ def main():
     pygame.display.set_caption('Flappy Crafts')
     clock = pygame.time.Clock()
 
-    # Game initialization
+    # Game initialization (prints background image/pipes/and DC)
     background_image = pygame.image.load('images/background.png').convert_alpha()
     pipes = pygame.image.load('images/pipes.png').convert_alpha()
     dc_logo = DC()
@@ -35,25 +31,37 @@ def main():
     while not stop_game:
         for event in pygame.event.get():
 
-            # Event handling
+            # Event handling for keystroke up for DC logo
+            if event.type == pygame.KEYDOWN:
+                if event.key == KEY_UP:
+                    dc_logo.dir_y -= 40
+                    dc_logo.image = pygame.image.load('images/DC-logo-tilt.png').convert_alpha()
+            if event.type == pygame.KEYUP:
+                if event.key == KEY_UP:
+                    dc_logo.dir_y = 15
+                    dc_logo.image = pygame.image.load('images/DC-logo.png').convert_alpha()
+
+            # stops the logo from going off the screen
             if dc_logo.y < 0:
                 dc_logo.y = 0
             if dc_logo.y >= 800:
                 dc_logo.y = 800
 
+            # quits game if red box clicked
             if event.type == pygame.QUIT:
                 stop_game = True
 
 
         # Game logic
-        dc_logo.y += dc_logo.dir_y
 
         # Draw background
         screen.blit(background_image, (0,0))
 
         # Game display
         dc_logo.draw(screen)
-        dc_logo.handle_keys()
+        dc_logo.y += dc_logo.dir_y
+
+        # dc_logo.handle_keys()
         screen.blit(pipes, (0,-400)),
         screen.blit(pipes, (100,-100)),
         screen.blit(pipes, (200,-200)),
