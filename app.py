1a62965b3135b405d3f111ab5f703b514ddd94a0
Matt Raines <mraines4@gmail.com>
2019-02-20 19:21:03 -0500

    add dc logo ability to move up

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 import pygame
 
+class DC(object):
+    def __init__(self):
+        self.image = pygame.image.load('images/DC-logo.png').convert_alpha()
+        self.x = 60
+        self.y = 400
+
+    def handle_keys(self):
+        key = pygame.key.get_pressed()
+        dist = 5
+        if key[pygame.K_UP]:
+            self.y -= dist
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
 def main():
     width = 800
     height = 800
 
     pygame.init()
     screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('My Game')
+    pygame.display.set_caption('Flappy Crafts')
     clock = pygame.time.Clock()
 
     # Game initialization
     background_image = pygame.image.load('images/background.png').convert_alpha()
-    dc_logo = pygame.image.load('images/DC-logo.png').convert_alpha()
     pipes = pygame.image.load('images/pipes.png').convert_alpha()
-
+    dc_logo = DC()
 
     stop_game = False
     while not stop_game:
@@ -31,7 +45,8 @@ def main():
         screen.blit(background_image, (0,0))
 
         # Game display
-        screen.blit(dc_logo, (60,400))
+        dc_logo.draw(screen)
+        dc_logo.handle_keys()
         screen.blit(pipes, (0,-400)),
         screen.blit(pipes, (100,-100)),
         screen.blit(pipes, (200,-200)),
